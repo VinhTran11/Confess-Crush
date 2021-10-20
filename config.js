9df0434fc6d93f13ac2dbd8b56547b36e8a61d08
VinhTran11 <92838533+VinhTran11@users.noreply.github.com>
2021-10-20 10:21:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,23 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Gửi Chi xđ",
+    introTitle: 'Phương Chi',
+    introDesc: `Nhân dịp 20 tháng 10, xin chúc chị:
+Trẻ trung như heo sữa
+Bốc lửa như heo quay
+Hăng say như heo con
+Sắc son như heo đất
+Đủ chất như...heo thịt
+Chúc mừng, chúc mừng!!!`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    btnReply: 'Gửi cho Chii <3',
+    reply: 'Tới ĐN thì tới mà không tới thì cũng phải tới <33333333',
+    mess: 'Mình biết mà 🥰. Mời bàn tới ĐN 😘😘',
+    messDesc: 'Tối nay 7h, qua đón mình đi nhậu nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
